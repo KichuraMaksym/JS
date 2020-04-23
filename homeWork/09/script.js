bc976df457c668fba012c6f7df911c82c606dbb4
KichuraMaksym <kichuramaksym@gmail.com>
2020-04-23 17:54:07 +0300

    9,1

--- homeWork/09/script.js
+++ homeWork/09/script.js
@@ -14,129 +14,164 @@
 // Кожна функція має мати якесь успіше або не успішне виконання. Тобто можливий варіант, що при виконанні функції
 // aдоїхати на роботу ви стали в заторі і не попали на роботу. Або йдучи на обід ви забули гаманець і лишились голодні.
 
-
-//BY PROMISE
-
-function wakeUp(isWorkDay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(function () {
-            if (isWorkDay) {
-                console.log('Pora prokidatis')
-                let vstaem = true
-                resolve(vstaem)
-            } else {
-                console.log('Siogodni vihidnii, mojna pospati')
-                reject(false)
-            }
-        }, 2000)
-    })
+let random = function () {
+    let x = Math.random();
+    return x + 0.3
 }
 
-wakeUp(true)
+// //BY PROMISE
+//
+// function wakeUp() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(function () {
+//             if (random() > 0.3) {
+//                 console.log('Pora prokidatis')
+//                 resolve(true)
+//             } else {
+//                 console.log('Siogodni vihidnii, mojna pospati')
+//
+//
+//             }
+//         }, 2000)
+//     })
+// }
+//
+// wakeUp()
+//
+//     .then(() => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(function () {
+//                 if (random() > 0.5) {
+//                     console.log('chistim zubi')
+//                     resolve(true)
+//                 } else console.log('zubna pasta kinchilas,sidim doma')
+//             }, 2000)
+//         })
+//     })
+//     .then(() => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(function () {
+//                 if (random() > 0.5) {
+//                     console.log('snidaem')
+//                     resolve(true)
+//                 } else console.log('Nema sho eat, tomu sidimo hungry')
+//             }, 2000)
+//         })
+//     })
+//     .then(function () {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(function () {
+//                 if (random() > 0.5) {
+//                     console.log('Odiagaemos')
+//                     resolve(true)
+//                 } else console.log('mojna zalipnuti v serialchiki')
+//             }, 2000)
+//         })
+//     })
+//     .then(function () {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (random() > 0.5) {
+//                     console.log('Sidaem na rover i pognali na galeri')
+//                     resolve(true)
+//                 } else console.log('Mojna shoditi v magaz kupiti pivko i jantarna s persem')
+//             }, 1500)
+//         })
+//     })
+//     .then(() => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (random() > 0.5) {
+//                     console.log('stvoruem robochii viglad, posmihaemos')
+//                     resolve(true)
+//                 } else console.log('Drink pivko divimosia serial')
+//             }, 1200)
+//         })
+//     })
+//     .then(value => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 console.log('idem na perekur')
+//                 resolve(true)
+//             }, 1500)
+//         })
+//     })
+//     .then(() => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (random() > 0.5) {
+//                     console.log('stvoruem robochii viglad, posmihaemos')
+//                     resolve(true)
+//                 } else console.log('perepisuemos v telegram')
+//             }, 1500)
+//         })
+//     })
+//     .then(value => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (random() > 0.5) {
+//                     console.log('Idem obidati')
+//                     resolve(true)
+//                 } else console.log('mojna pohavati')
+//             }, 1000)
+//         })
+//     })
+//     .then(value => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (random() > 0.5) {
+//                     console.log('vertaemsia do roboti')
+//                     resolve(true)
+//                 } else console.log('teper posluhae music')
+//             }, 2300)
+//         })
+//     })
+//     .then(value => {
+//         return new Promise(resolve => {
+//             setTimeout(() => {
+//                 if (random() > 0.5) {
+//                     console.log('Vertaemsia dodomu, vecheriaem, lagaem spati')
+//                 } else console.log('mojna lagati spati')
+//             }, 1777)
+//         })
+//     })
+//     .catch(reason => {
+//         console.log(reason)
+//     })
 
-    .then((vstaem) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(function () {
-                if (vstaem) {
-                    console.log('chistim zubi')
-                    let snidaem = true
-                    resolve(snidaem)
-                }
-            }, 2000)
-        })
-    })
-    .then((snidaem) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(function () {
-                if (snidaem) {
-                    console.log('snidaem')
-                    let Odiagaemos = true;
-                    resolve(Odiagaemos)
-                }
-            }, 2000)
-        })
-    })
-    .then(function (Odiagaemos) {
-        return new Promise((resolve, reject) => {
-            setTimeout(function () {
-                if (Odiagaemos) {
-                    console.log('Odiagaemos')
-                    let Odiagaemos = true
-                    resolve(Odiagaemos)
-                }
-            }, 2000)
-        })
-    })
-    .then(function (Odiagaemos) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (Odiagaemos) {
-                    console.log('Sidaem na rover i pognali na galeri')
-                    let value = true
-                    resolve(value)
-                }
-            }, 1500)
-        })
-    })
-    .then((value) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (value) {
-                    console.log('stvoruem robochii viglad, posmihaemos')
-                    let value = true
-                    resolve(value)
-                }
-            }, 1200)
-        })
-    })
-    .then(value => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                console.log('idem na perekur')
-                let value = true
-                resolve(value)
-            }, 1500)
-        })
-    })
-    .then((value) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (value) {
-                    console.log('stvoruem robochii viglad, posmihaemos')
-                    let value = true
-                    resolve(value)
-                }
-            }, 1500)
-        })
-    })
-    .then(value => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (value) {
-                    console.log('Idem obidati')
-                    let value = true
-                    resolve(value)
-                }
-            }, 1000)
-        })
-    })
-    .then(value => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (value) {
-                    console.log('vertaemsia do roboti')
-                    let value = true
-                    resolve(value)
-                }
-            }, 2300)
-        })
-    })
-.then(value => {
-    return new Promise(resolve => {
-        setTimeout(()=>{
-            if (value){
-                console.log('Vertaemsia dodomu, vecheriaem, lagaem spati')
-            }
-        },1777)
-    })
-})
+// BY CALLBACK
+
+let money = 500; //Berem z soboju zranku ${money} griven.
+
+let vakeUp = function () {
+    setTimeout(function () {
+        console.log('Prokidaemos, miemos,briemos,snidaem')
+        goWork()
+    }, 2777)
+
+}
+
+let goWork = function (cb) {
+    setTimeout(function () {
+        if (money >= 8) {
+            money -=8
+            console.log('sidaem na marshrytku odem na robotu')
+            workHard()
+        } else {
+            console.log('Idem na pobotu pishkom bo nema groshey')
+            workHard()
+        }
+
+
+    }, 2000)
+}
+
+let workHard = function () {
+    setTimeout(function () {
+        if (money > 100500) {
+            console.log('Mojna halturiti,groshei dostatnio, bilse ne treba')
+        } else console.log('Treba arbaiten, treba zarobliati')
+    }, 2000)
+}
+
+vakeUp();
